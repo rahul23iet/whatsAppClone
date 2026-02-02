38f4766838094caabb89686b17562c18237720b2
rahul <rahul23iet@gmail>
2026-02-02 10:24:27 +0530

    working on mailservice

---
+++ backend/mail/src/consumer.ts
@@ -0,0 +1,4 @@
+import amqplib from 'amqplib';
+import dotenv from 'dotenv';
+dotenv.config();
+
--- backend/users/src/config/rabbitmq.ts
+++ backend/users/src/config/rabbitmq.ts
@@ -11,12 +11,17 @@ export const connectRabbitMQ = async () => {
             username: process.env.RABBITMQ_USER || 'guest',
             password: process.env.RABBITMQ_PASS || 'guest',
         });
-
         channel = await connection.createChannel();
-
         console.log("Connected to RabbitMQ");
-         
     } catch (error) {
         console.error("RabbitMQ connection error:", error);
     }
+}
+
+export const publishToQueue = async (queue: string, message: string) => {
+     if(!channel){
+        throw new Error("RabbitMQ channel is not established. Call connectRabbitMQ first.");
+     }
+     await channel.assertQueue(queue, { durable: true });
+     channel.sendToQueue(queue, Buffer.from(message), { persistent: true });
 }
